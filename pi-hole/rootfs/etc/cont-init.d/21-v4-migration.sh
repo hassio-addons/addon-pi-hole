8184e670cc88b00f99ae7eb5f047248625e1e2db
Franck Nijhof <frenck@addons.community>
2018-08-22 20:41:26 +0200

    :sparkles: Adds Pihole vX -> v4 migration script

---
+++ pi-hole/rootfs/etc/cont-init.d/21-v4-migration.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Community Hass.io Add-ons: Pi-hole
+# Handles the migration of existing installation to v4
+# ==============================================================================
+# shellcheck disable=SC1091
+source /usr/lib/hassio-addons/base.sh
+
+# If no data is detected, skip this part
+if ! hass.directory_exists '/data/pihole'; then
+    exit "${EX_OK}"
+fi
+
+# Copy "possible" missing files, due to upgrades
+if ! hass.file_exists '/data/pihole/pihole-FTL.conf'; then
+    cp /etc/pihole/pihole-FTL.conf /data/pihole/pihole-FTL.conf
+fi
